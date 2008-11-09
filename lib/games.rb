1e98d72947b6e520e1142d0731b355c0fe736dcb
Joosa Riekkinen <joosa@heh.fi>
2008-11-10 01:26:56 +0200

    rcfiles

--- lib/games.rb
+++ lib/games.rb
@@ -1,11 +1,11 @@
 require "date"
 require "server"
-require "escape"
 
 module Games
   def self.crawl(user)
     ttyrec = user + " " + DateTime.now.to_s + ".ttyrec"
-    system Escape.shell_command(["ttyrec ", Server::SERVER_DIR + "inprogress/" + ttyrec,"-e","/usr/games/crawl -name " + user])
-    system "apack \"" + Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz\" \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\""
+    system "ttyrec", Server::SERVER_DIR + "inprogress/" + ttyrec, "-e", "/usr/games/crawl -name " + user + "-rc " + Server::SERVER_DIR + "rcfiles/" + user + ".crawlrc"
+    system "apack", Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz", Server::SERVER_DIR + "inprogress/" + ttyrec
+    system "rm", Server::SERVER_DIR + "inprogress/" + ttyrec
   end
 end
