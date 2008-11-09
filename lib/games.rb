2cdd92605f2550e711b67f49bbbfef08a07d1ef8
Joosa Riekkinen <joosa@heh.fi>
2008-11-10 00:09:18 +0200

    fix to previous commit

--- lib/games.rb
+++ lib/games.rb
@@ -3,7 +3,7 @@ require "server"
 
 module Games
   def self.crawl(user)
-    ttyrec = user + "\\ " + DateTime.now.to_s + ".ttyrec"
+    ttyrec = user + " " + DateTime.now.to_s + ".ttyrec"
     system "ttyrec \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\" -e \"/usr/games/crawl -name " + user + "\""
     system "apack \"" + Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz\" \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\""
   end
