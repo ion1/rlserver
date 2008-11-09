9b6801bfda0217dfec6f4b93e10254f40c7c6570
Joosa Riekkinen <joosa@heh.fi>
2008-11-09 23:57:29 +0200

    ttyrex fix

--- lib/games.rb
+++ lib/games.rb
@@ -4,7 +4,7 @@ require "server"
 module Games
   def self.crawl(user)
     ttyrec = Server::SERVER_DIR + "inprogress/" + user + "\\ " + DateTime.now.to_s + ".ttyrec"
-    system "ttyrec " + ttyrec + " -e \"crawl -name " + user + "\""
+    system "ttyrec " + ttyrec + " -e \"/usr/games/crawl -name " + user + "\""
     system "mv " + ttyrec + " ttyrec"
   end
 end
