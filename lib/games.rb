7b23c79d1c8c35659b2e4495ed056e02e1b63ab7
Joosa Riekkinen <joosa@heh.fi>
2008-11-10 00:08:03 +0200

    gzipped ttyrecs

--- lib/games.rb
+++ lib/games.rb
@@ -5,6 +5,6 @@ module Games
   def self.crawl(user)
     ttyrec = user + "\\ " + DateTime.now.to_s + ".ttyrec"
     system "ttyrec \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\" -e \"/usr/games/crawl -name " + user + "\""
-    system "apack \"" Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz\" \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\""
+    system "apack \"" + Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz\" \"" + Server::SERVER_DIR + "inprogress/" + ttyrec + "\""
   end
 end
