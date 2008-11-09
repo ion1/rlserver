e9989df9066124c8859289b77c5f3022a1666659
Joosa Riekkinen <joosa@heh.fi>
2008-11-10 01:33:13 +0200

    /dev/null

--- lib/games.rb
+++ lib/games.rb
@@ -5,7 +5,7 @@ module Games
   def self.crawl(user)
     ttyrec = user + " " + DateTime.now.to_s + ".ttyrec"
     system "ttyrec", Server::SERVER_DIR + "inprogress/" + ttyrec, "-e", "/usr/games/crawl -name " + user + " -rc " + Server::SERVER_DIR + "rcfiles/" + user + ".crawlrc"
-    system "apack", Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz", Server::SERVER_DIR + "inprogress/" + ttyrec
-    system "rm", Server::SERVER_DIR + "inprogress/" + ttyrec
+    system "apack", Server::SERVER_DIR + "ttyrec/" + ttyrec + ".tgz", Server::SERVER_DIR + "inprogress/" + ttyrec, ">", "/dev/null"
+    system "rm", Server::SERVER_DIR + "inprogress/" + ttyrec, ">", "/dev/null"
   end
 end
