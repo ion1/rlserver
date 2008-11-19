b0e7f3dc5ce95c8774a63cc22b683a9dd9c4dffe
Joosa Riekkinen <joosa@heh.fi>
2008-11-19 10:37:53 +0200

    more style editing.

--- lib/scores.rb
+++ lib/scores.rb
@@ -55,22 +55,22 @@ module Scores
     html = <<HTML_END
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
-<head>
-  <title>Crawl Scores</title>
-  <script type="text/javascript" src="sortabletable/js/sortabletable.js"></script>
-  <link rel="stylesheet" type="text/css" href="sortabletable/css/sortabletable.css"/>
-  <link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
-	<table>
-		<tr align="left"><th>#</th><th>Name</th><th>Player Points</th><th>Bonus Points</th><th>Total Score</th><th>Best</th></tr>
-		#{i=0;score.total_points.sort{|x,y|y[1] <=> x[1]}.map{|x| "<tr><td>#{i+=1}</td><td>#{x[0]}</td><td>#{score.player_points[x[0]].to_i}</td><td>#{score.bonus_points[x[0]].to_i*score.bonus_mult}</td><td>#{x[1]}</td><td>#{score.bonuses[x[0]].join(", ")}</td></tr>"}.join("\n")}
-	</table>
-	<table>
-		<tr align="left"><th>#</th><th>Name</th><th>Race/Class</th><th>HP</th><th>Dungeon</th><th>Score</th><th>Killer</th><th>with</th></tr>
-		#{i=0;score.data.map{|points| "<tr><td>#{i+=1}</td><td>#{points["name"]}</td><td>#{points["race"]} #{points["cls"]} (lvl:#{points["xl"]})</td><td>#{points["hp"]}/#{points["mhp"]}</td><td>#{points["br"]}:#{points["lvl"]}<td>#{points["sc"]}</td><td>#{points["killer"]}</td><td>#{points["kaux"]}</td></tr>"}.join("\n")}
-	</table>
-</body>
+  <head>
+    <title>Crawl Scores</title>
+    <script type="text/javascript" src="sortabletable/js/sortabletable.js"></script>
+    <link rel="stylesheet" type="text/css" href="sortabletable/css/sortabletable.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <table>
+      <thead><tr align="left"><th>#</th><th>Name</th><th>Player Points</th><th>Bonus Points</th><th>Total Score</th><th>Best</th></tr></thead>
+      <tbody>#{i=0;score.total_points.sort{|x,y|y[1] <=> x[1]}.map{|x| "<tr><td>#{i+=1}</td><td>#{x[0]}</td><td>#{score.player_points[x[0]].to_i}</td><td>#{score.bonus_points[x[0]].to_i*score.bonus_mult}</td><td>#{x[1]}</td><td>#{score.bonuses[x[0]].join(", ")}</td></tr>"}.join("\n")}</tbody>
+    </table>
+    <table>
+      <thead><tr align="left"><th>#</th><th>Name</th><th>Race/Class</th><th>HP</th><th>Dungeon</th><th>Score</th><th>Killer</th><th>with</th></tr></thead>
+      <tbody>#{i=0;score.data.map{|points| "<tr><td>#{i+=1}</td><td>#{points["name"]}</td><td>#{points["race"]} #{points["cls"]} (lvl:#{points["xl"]})</td><td>#{points["hp"]}/#{points["mhp"]}</td><td>#{points["br"]}:#{points["lvl"]}<td>#{points["sc"]}</td><td>#{points["killer"]}</td><td>#{points["kaux"]}</td></tr>"}.join("\n")}</tbody>
+    </table>
+  </body>
 </html>
 HTML_END
     File.open(CRAWL_HTML, "w") {|file| file.write(html)}
