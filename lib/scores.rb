268d52a06e273a523160d3a4840d3228a7846213
Joosa Riekkinen <joosa@heh.fi>
2008-11-19 10:50:07 +0200

    more styles.

--- lib/scores.rb
+++ lib/scores.rb
@@ -67,11 +67,53 @@ module Scores
       <thead><tr align="left"><th>#</th><th>Name</th><th>Player Points</th><th>Bonus Points</th><th>Total Score</th><th>Best</th></tr></thead>
       <tbody>#{i=0;score.total_points.sort{|x,y|y[1] <=> x[1]}.map{|x| "<tr class=#{COLORS[i % 2]}><td>#{i+=1}</td><td>#{x[0]}</td><td>#{score.player_points[x[0]].to_i}</td><td>#{score.bonus_points[x[0]].to_i*score.bonus_mult}</td><td>#{x[1]}</td><td>#{score.bonuses[x[0]].join(", ")}</td></tr>"}.join("\n")}</tbody>
     </table>
-    <table>
+    <table class="sort-table">
       <thead><tr align="left"><th>#</th><th>Name</th><th>Race/Class</th><th>HP</th><th>Dungeon</th><th>Score</th><th>Killer</th><th>with</th></tr></thead>
       <tbody>#{i=0;score.data.map{|points| "<tr class=#{COLORS[i % 2]}><td>#{i+=1}</td><td>#{points["name"]}</td><td>#{points["race"]} #{points["cls"]} (lvl:#{points["xl"]})</td><td>#{points["hp"]}/#{points["mhp"]}</td><td>#{points["br"]}:#{points["lvl"]}<td>#{points["sc"]}</td><td>#{points["killer"]}</td><td>#{points["kaux"]}</td></tr>"}.join("\n")}</tbody>
     </table>
-  </body>
+    <script type="text/javascript">
+    //<![CDATA[
+    function addClassName(el, sClassName)
+    {
+        var s = el.className;
+        var p = s.split(" ");
+        var l = p.length;
+        for (var i = 0; i < l; i++)
+        {
+            if (p[i] == sClassName)
+            return;
+        }
+        p[p.length] = sClassName;
+        el.className = p.join(" ").replace( /(^\\s+)|(\\s+\$)/g, "" );
+    }
+    function removeClassName(el, sClassName)
+    {
+        var s = el.className;
+        var p = s.split(" ");
+        var np = [];
+        var l = p.length;
+        var j = 0;
+        for (var i = 0; i < l; i++)
+        {
+            if (p[i] != sClassName)
+            np[j++] = p[i];
+        }
+        el.className = np.join(" ").replace( /(^\\s+)|(\\s+\$)/g, "" );
+    }
+    var st1 = new SortableTable(document.getElementById("table-1"),["Number", "CaseInsensitiveString", "Number", "Number", "Number", "CaseInsensitiveString", "CaseInsensitiveString"]);
+    st1.onsort = function ()
+    {
+        var rows = st1.tBody.rows;
+        var l = rows.length;
+        for (var i = 0; i < l; i++)
+        {
+            removeClassName(rows[i], i % 2 ? "odd" : "even");
+            addClassName(rows[i], i % 2 ? "even" : "odd");
+        }
+    };
+    //]]>
+    </script>
+    </body>
 </html>
 HTML_END
     File.open(CRAWL_HTML, "w") {|file| file.write(html)}
