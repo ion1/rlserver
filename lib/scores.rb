fb9f645834b4d176b97b2d0547ac65cbf4ce35b9
Joosa Riekkinen <joosa@heh.fi>
2008-11-19 10:52:46 +0200

    more styles.

--- lib/scores.rb
+++ lib/scores.rb
@@ -63,14 +63,18 @@ module Scores
     <link rel="stylesheet" type="text/css" href="style.css"/>
   </head>
   <body>
-    <table>
+    <p>
+    <table class="sort-table">
       <thead><tr align="left"><th>#</th><th>Name</th><th>Player Points</th><th>Bonus Points</th><th>Total Score</th><th>Best</th></tr></thead>
       <tbody>#{i=0;score.total_points.sort{|x,y|y[1] <=> x[1]}.map{|x| "<tr class=#{COLORS[i % 2]}><td>#{i+=1}</td><td>#{x[0]}</td><td>#{score.player_points[x[0]].to_i}</td><td>#{score.bonus_points[x[0]].to_i*score.bonus_mult}</td><td>#{x[1]}</td><td>#{score.bonuses[x[0]].join(", ")}</td></tr>"}.join("\n")}</tbody>
     </table>
+    </p>
+    <p>
     <table class="sort-table">
       <thead><tr align="left"><th>#</th><th>Name</th><th>Race/Class</th><th>HP</th><th>Dungeon</th><th>Score</th><th>Killer</th><th>with</th></tr></thead>
       <tbody>#{i=0;score.data.map{|points| "<tr class=#{COLORS[i % 2]}><td>#{i+=1}</td><td>#{points["name"]}</td><td>#{points["race"]} #{points["cls"]} (lvl:#{points["xl"]})</td><td>#{points["hp"]}/#{points["mhp"]}</td><td>#{points["br"]}:#{points["lvl"]}<td>#{points["sc"]}</td><td>#{points["killer"]}</td><td>#{points["kaux"]}</td></tr>"}.join("\n")}</tbody>
     </table>
+    </p>
     <script type="text/javascript">
     //<![CDATA[
     function addClassName(el, sClassName)
