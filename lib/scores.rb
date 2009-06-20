d6ae58b6f8f34d92a4622dc334e6c3f77b034c1f
Joosa Riekkinen <joosa@heh.fi>
2009-06-20 19:38:16 +0300

    update crawl scores

--- lib/scores.rb
+++ lib/scores.rb
@@ -1,5 +1,5 @@
-CRAWL_FILENAME = "/var/games/saves/scores"
-CRAWL_HTML = "/home/www/rlserver/public/crawl.html"
+CRAWL_FILENAME = "/var/games/crawl/saves/scores"
+CRAWL_HTML = "crawl.html"
 COLORS = ["odd", "even"]
 
 module Scores
