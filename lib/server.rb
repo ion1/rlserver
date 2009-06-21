2e72301902616472f1b7ccca259e2330bc6d7034
Joosa Riekkinen <joosa@heh.fi>
2009-06-21 13:13:26 +0300

    finally fix hanging on disconnect

--- lib/server.rb
+++ lib/server.rb
@@ -1,14 +1,4 @@
 module Server
   SERVER_DIR = "/home/shared/joosa/src/rlserver/"
   SERVER_USER = "joosa"
-  def self.initialize
-    @pid = Process.pid
-    @ppid = Process.ppid
-  end
-  def pid
-    @pid
-  end
-  def ppid
-    @ppid
-  end
 end
