a58d7b872ecaafc44932a0658343a8afc895444c
Joosa Riekkinen <joosa@heh.fi>
2009-06-27 13:24:06 +0300

    Watch menu adapted for the new menu system
    Also, worp wrap using regexp and some fixes.

--- lib/menu.rb
+++ lib/menu.rb
@@ -73,56 +73,41 @@ module Menu
   #stuff is beginning to weep through to places it shouldn't be
   #refactoring ensues in the future
   ATTRIB = {"b" => Ncurses::A_BOLD, "r" => Ncurses::A_REVERSE, "n" => Ncurses::A_NORMAL, "s" => Ncurses::A_STANDOUT}
+
+  def self.wrap_text(txt, col = 80)
+    txt.gsub /(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n"
+  end
+
   def self.aputs(win, s)
     control = false
     attrs = ""
-    line_count = s.each_line.collect.size
-    #win.printw "#{line_count}"
-    line_num = 0
-    s.each_line do |line|
-      word_num = 0
-      line.split(" ").each do |word|
-        if win.getcurx > 0 and word_num > 0 then
-          if win.getcurx + word.length >= win.getmaxx then
-            win.printw "\n"
+    if s.length > win.getmaxx then s = wrap_text(s, win.getmaxx) end
+    s.each_char do |c|
+      if control then
+        control = false
+        if ATTRIB.key? c then
+          if attrs.include? c
+            win.attroff ATTRIB[c]
+            attrs.delete! c
           else
-            win.printw " "
+            win.attron ATTRIB[c]
+            attrs += c
           end
-        end
-        word.each_char do |c|
-          if control then
-            control = false
-            if ATTRIB.key? c then
-              if attrs.include? c
-                win.attroff ATTRIB[c]
-                attrs.delete! c
-              else
-                win.attron ATTRIB[c]
-                attrs += c
-              end
-            elsif
-              case c when "0".."9":
-                if attrs.include? c
-                  win.attroff Ncurses.COLOR_PAIR c.to_i
-                  attrs.delete! c
-                else
-                  win.attron Ncurses.COLOR_PAIR c.to_i
-                  attrs += c
-                end
-              end
+        elsif
+          case c when "0".."9":
+            if attrs.include? c
+              win.attroff Ncurses.COLOR_PAIR c.to_i
+              attrs.delete! c
             else
-              win.addch c[0]
+              win.attron Ncurses.COLOR_PAIR c.to_i
+              attrs += c
             end
-          else
-            (control = (c == "$")) ? () : (win.addch c[0])
           end
+        else
+          win.addch c[0]
         end
-        word_num += 1
-      end
-      line_num += 1
-      #win.printw "#{line_num}"
-      if line_num < line_count then
-        win.printw "\n"
+      else
+        (control = (c == "$")) ? () : (win.addch c[0])
       end
     end
   end
@@ -272,19 +257,25 @@ module Menu
     row = win.getcury
     keys = {}
     choices.each do |key, choice, block|
-      key.each_byte do |k|
-        keys[k] = block
+      if key.class == Fixnum then
+        keys[key] = block
+      else
+        key.each_byte do |k|
+          keys[k] = block
+        end
       end
       win.move row, 0
-      aputs win, "$b#{key ? "#{key[0,1]}$b - " : "    "}#{choice}"
-      row += 1
+      if choice then
+        aputs win, "#{key ? "$b#{key[0,1]}$b - " : "    "}#{choice}"
+        row += 1
+      end
     end
     Ncurses::Panel.update_panels
     Ncurses.doupdate
     key = win.getch
     Ncurses.cbreak
     if keys.key? key then
-      keys[key].call
+      keys[key].call key
     end
   end
   
@@ -350,17 +341,18 @@ module Menu
     "%02d:%02d:%02d" % [h, m, s]
   end
 
+  GAME_COLOR = { true => "$b$3", false => "$3" }
   def self.watch
     win = Ncurses::Panel.panel_window @menu_panel
     quit = false
     offset = 0
     sel = 0
-    chars = "abcdefghijklmnop"
+    chars = "abcdefghijklmnopABCDEFGHIJKLMNOP"
     while !quit do
-      pagesize = win.getmaxy - 4
-      if pagesize > 16 then pagesize = 16 end
       title "Watch games"
       status "$bPage Up$b / $bPage Down$b - scroll, $bq$b - back"
+      win.clear
+      aputs win, "While watching, press $bq$b to return here.\nThe screen may appear garbled if the terminal is too small (will try to resize automatically).\n\n"
       Games.populate
       pretty = []
       active_games = []
@@ -377,25 +369,38 @@ module Menu
       detached = detached_games.length
       total = active_games + detached_games
       total.each do |game|
-        a = game.attached ? "$b$3" : "$3"
-        pretty += ["%s%-14s%-14s%-14s(idle %s)%s" % [a, game.player, Config.config["games"][game.game]["name"], "(%3ux%3u)" % [game.cols, game.rows], mktime(game.idle), a]]
+        pretty += ["%s%-14s%-20s%-14s(idle %s)%s" % [GAME_COLOR[game.attached], game.player, Config.config["games"][game.game]["name"], "(%3ux%3u)" % [game.cols, game.rows], mktime(game.idle), GAME_COLOR[game.attached]]]
       end
-      win.clear
-      aputs win,
-        "Use $buppercase$b to try to resize the terminal (recommended).\n" +
-        "While watching, press $bq$b to return to the menu.\n"
       if total.length > 0 then
-        aputs win, "Showing games #{offset+1}-#{(((offset+pagesize) > total.length) ? total.length : offset+pagesize)} of #{total.length} ("
+        aputs win, "Currently running games ("
         if active > 0 then
-          aputs win, "$b$3#{active} active$b$3#{(detached > 0 ? ", " : "")}"
+          aputs win, "$b$3#{active} active$b$3#{detached > 0 ? ", " :  ""}"
         end
         if detached > 0 then
           aputs win, "$3#{detached} detached$3"
         end
-        aputs win, ")."
+        aputs win, "):"
+        pagesize = win.getmaxy - win.getcury - 2
+        if pagesize > 32 then pagesize = 32 end
         offset.upto(offset + pagesize - 1) do |i|
           if i < total.length then
-            socketmenu += [[total[i].attached ? "$b#{chars[i % pagesize, 1]}$b" : nil, pretty[i]]]
+            launch = lambda do |key|
+              case key
+              when "a"[0].."p"[0]: sel = key - 97
+              when "A"[0].."P"[0]: sel = key - 81
+              end
+              if sel < active then
+                Ncurses.def_prog_mode
+                destroy
+                puts "\033[8;#{active_games[sel].rows};#{active_games[sel].cols}t"
+                Games.watchgame active_games[sel].socket
+                initncurses
+                Ncurses.reset_prog_mode
+                resize
+              end
+              false
+            end
+            socketmenu += [[total[i].attached ? "#{chars[i % pagesize, 1]}" : nil , pretty[i], launch]]
           end
         end
         items = socketmenu.length
@@ -403,74 +408,18 @@ module Menu
         aputs win, "There are no games running."
       end
       win.printw "\n\n"
-      sel = menu false, *socketmenu
-      case sel
-      when Ncurses::KEY_PPAGE: 
-        offset -= pagesize
-        if offset < 0 then offset = 0 end
-      when Ncurses::KEY_NPAGE:
-        offset += pagesize
-        if offset >= total.length then offset -= pagesize end
-      when "a".."p":
-        sel = offset + sel[0]-97
-        if sel < active then
-          Ncurses.def_prog_mode
-          destroy
-          Games.watchgame active_games[sel].socket
-          initncurses
-          Ncurses.reset_prog_mode
-          resize
-        end
-      when "A".."P":
-        sel = offset + sel[0] - 65
-        if sel < active then
-          Ncurses.def_prog_mode
-          destroy
-          puts "\033[8;#{active_games[sel].rows};#{active_games[sel].cols}t"
-          Games.watchgame active_games[sel].socket
-          initncurses
-          Ncurses.reset_prog_mode
-          resize
-        end
-      when "q", "Q": quit = true
-      end
+      socketmenu += [
+        ["qQ", nil, lambda {true}],
+        [Ncurses::KEY_PPAGE, nil, lambda {offset -= pagesize; if offset < 0 then offset = 0 end; false}],
+        [Ncurses::KEY_NPAGE, nil, lambda {offset += pagesize; if offset >= total.length then offset -= pagesize end; false}]]
+      quit = menu socketmenu
     end
   end
 
-  #keeping these here just for the parameters
-
-#  def self.angbandmenu
-#    quit = false
-#    while !quit do
-#      case menu(
-#      "p - Play Angband",
-#      "e - Edit configuration file",
-#      "q - Quit")
-#      when "p"[0], "P"[0]:
-#        Games.populate
-#        Games.launchgame @user, "/usr/games/angband", "Angband", "-mgcu -u\"" + @user + "\"", [["SHELL", "/bin/sh"]]
-#      when "e"[0], "E"[0]:
-#      when "q"[0], "Q"[0]: quit = true
-#      end
-#    end
-#  end
-#
-#  def self.nethackmenu
-#    quit = false
-#    while !quit do
-#      case menu(
-#      "p - Play NetHack",
-#      "e - Edit configuration file",
-#      "q - Quit")
-#      when "p"[0], "P"[0]:
-#        Games.populate
-#        Games.launchgame @user, "/usr/games/nethack", "NetHack", "-u \"" + @user + "\"", [["NETHACKOPTIONS", File.expand_path("rcfiles/" + @user + ".nethack")],["SHELL", "/bin/sh"]]
-#      when "e"[0], "E"[0]: Games.editrc @user, "nethack"
-  #      when "q"[0], "Q"[0]: quit = true
-  #      end
-  #    end
-  #  end
-
+  # keeping these here just for the parameters
+  #        Games.launchgame @user, "/usr/games/angband", "Angband", "-mgcu -u\"" + @user + "\"", [["SHELL", "/bin/sh"]]
+  #        Games.launchgame @user, "/usr/games/nethack", "NetHack", "-u \"" + @user + "\"", [["NETHACKOPTIONS", File.expand_path("rcfiles/" + @user + ".nethack")],["SHELL", "/bin/sh"]]
+  #        
   def self.gamemenu(game)
     quit = false
     win = Ncurses::Panel.panel_window @menu_panel
@@ -496,7 +445,7 @@ module Menu
       win.clear
       status gen_status
       title "#{Config.config["games"][game]["name"]} #{Config.config["games"][game]["version"]}#{(@count > 0) ? ((Games.by_user[@user].key? game) ? " (running)" : "") : ""} "
-      aputs win, Config.config["games"][game]["description"] + "\n\n\n"
+      aputs win, Config.config["games"][game]["description"] + "\n\n"
       quit = menu([["pP", "Play #{Config.config["games"][game]["name"]}", launch],
                   ["eE", "Edit configuration file", edit],
                   ["qQ", "Back", lambda {true}]])
@@ -525,7 +474,7 @@ module Menu
       title "Main menu"
       status gen_status
       win.clear
-      aputs win, Config.config["server"]["banner"] + "\n\n\n"
+      aputs win, Config.config["server"]["banner"] + "\n\n"
       quit =
         if @user then
           menu([["pP", "Change password", lambda {change_password; false}],
